1e944fc36f61bf52837206770351a37c896a0260
Kenneth Cornett <kecornett42@students.tntech.edu>
2017-03-07 17:43:33 -0600

    Swapped out for a previous designed class with better functionality.

--- include/Matrix.h
+++ include/Matrix.h
@@ -1,30 +1,62 @@
-#ifndef MATRIX_H
-#define MATRIX_H
+#ifndef MATRIX_H_
+#define MATRIX_H_
+// Matrix Class
+/*
+ *	Author:		Kenneth Cornett (Aero)
+ *	Namespace:	AeroSW
+ *	File:		matrix.h
+ */
+#include <string>
+#include <vector>
 
-#include <cmath>
-#include "TypeDefs.h"
-
-class matrix{
-	private:
-		double * mat; //! The array to the content of this matrix in.
-		uint rows;
-		uint cols;
-		
-	public:
-		matrix(uint r, uint c, double v=0.0);
-		matrix(const matrix &matr);
-		~matrix();
-		
-		matrix add(const matrix &matr);
-		matrix dot(const matrix &matr);
-		matrix sub(const matrix &matr);
-		matrix transpose();
-		
-		double get_element(uint r, uint c);
-		void set_element(uint r, uint c, double v);
-		
-		uint num_rows();
-		uint num_cols();
-};
-
+namespace AeroSW{
+	typedef unsigned int uint;
+	class matrix{
+		private:
+			//std::vector<double> m;
+			double * m;
+			uint num_rows;
+			uint num_cols;
+			uint num_elem;
+			
+		public:
+			matrix(); // default array, does nothing since there are no parameters.
+			matrix(const matrix &mat);
+			matrix(uint r, uint c);
+			matrix(uint r, uint c, double * itms);
+			
+			~matrix();
+			
+			double get_elem(uint id);
+			double get_elem(uint r, uint c);
+			
+			matrix add(matrix &mat);
+			//matrix cross(matrix &mat);
+			matrix dot(matrix &mat);
+			/* Returns an identity matrix with dimensions nxn */
+			static matrix ident(uint n);
+			/* invert uses the Gauss-Jordan elimination or Augmented Matrix Method to find inverse. */
+			matrix invert(); // to-do
+			matrix scalar_add(double scalar);
+			matrix scalar_add(long long scalar);
+			matrix scalar_mult(double scalar);
+			matrix scalar_mult(long long scalar);
+			matrix scalar_sub(double scalar);
+			matrix scalar_sub(long long scalar);
+			matrix sub(matrix &mat);
+			matrix transpose();
+			
+			std::string to_string();
+			static std::string get_type();
+			
+			uint * get_dim();
+			uint row_count();
+			uint col_count();
+			
+			void set_elem(uint index, double val);
+			void set_elem(uint r, uint c, double val);
+			
+			matrix& operator=(const matrix &matr);
+	};
+}
 #endif
--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -1,110 +1,299 @@
+/* matrix.cpp */
+/*
+ *	Author:		Kenneth Cornett (Aero)
+ *	Namespace:	AeroSW
+ *	File:		matrix.cpp
+ */
+
 #include "Matrix.h"
+#include <cmath>
+#include <string>
 #include "omp.h"
 
-matrix::matrix(uint r, uint c, double v):
-rows(r),cols(c){
-	mat = new double[r*c](v);
-}
-matrix::matrix(const matrix &matr):
-rows(matr.rows),cols(matr.cols){
-	mat = new double[rows * cols];
-	uint dx = rows * cols;
-	#pragma omp parallel for schedule(dynamic)
-	for(uint cx = 0; cx < dx; cx++){
-		mat[cx] = matr.mat[cx];
+namespace AeroSW{
+	typedef unsigned int uint;
+	
+	matrix::matrix()/*:
+	m(2)*/{
+		num_rows = 0;
+		num_cols = 0;
+		num_elem = 0;
+		m = new double[2];
 	}
-}
-
-matrix::~matrix(){
-	delete mat;
-}
-
-matrix matrix::add(const matrix &matr){
-	res_rows = rows;
-	res_cols = cols;
-	if(matr.rows > res_rows){
-		res_rows = matr.rows;
-	}
-	if(matr.cols > res_cols){
-		res_cols = matr.cols;
-	}
-	matrix result(res_rows, res_cols);
-	#pragma omp parallel for schedule(dynamic)
-	for(uint r = 0; r < res_rows; r++){
-		for(uint c = 0; c < res_cols; c++){
-			double v = get_element(r, c) + matr.get_element(r, c); // using get_element so if we go out of bounds, a 0 is returned.
-			result.set_element(r, c, v);
+	matrix::matrix(const matrix &mat) :
+	num_rows(mat.num_rows),num_cols(mat.num_cols){
+		num_elem = num_rows * num_cols;
+		m = new double[num_elem];
+		for(uint i = 0; i < num_elem; i++){
+			m[i] = mat.m[i];
 		}
 	}
-	return result;
-}
-
-matrix matrix::dot(const matrix &matr){
-	uint res_rows = rows;
-	uint res_cols = matr.cols;
-	uint m = cols;
-	if(m < matr.rows){
-		m = matr.rows;
+	matrix::matrix(uint r, uint c)/*:
+	m(r*c)*/{
+		num_rows = r;
+		num_cols = c;
+		num_elem = r * c;
+		m = new double[r * c];
 	}
-	matrix result(res_rows, res_cols);
-	#pragma omp parallel for schedule(dynamic)
-	for(uint r = 0; r < res_rows; r++){
-		for(uint c = 0; c < res_cols; c++){
-			double summ(0.0);
-			for(uint k = 0; k < m; k++){
-				summ += (get_element(r, k) * matr.get_element(k, c));
+	matrix::matrix(uint r, uint c, double * contents)/*:
+	m(r*c)*/{
+		// Assume contents size is r * c
+		num_rows = r;
+		num_cols = c;
+		num_elem = r * c;
+		m = new double[num_elem];
+		for(uint i = 0; i < num_elem; i++){
+			m[i] = contents[i];
+		}
+	}
+	
+	matrix::~matrix(){
+		delete[] m;
+	}
+	
+	double matrix::get_elem(uint index){
+		if(index > num_elem){
+			return 0.0;
+		}
+		return m[index];
+	}
+	double matrix::get_elem(uint r, uint c){
+		if(r >= num_rows || c >= num_cols){
+			return 0.0;
+		}
+		uint index = (r * num_cols) + c;
+		return m[index];
+	}
+	
+	matrix matrix::add(matrix &mat){
+		uint mrows = mat.row_count();
+		uint mcols = mat.col_count();
+		uint nrows;
+		uint ncols;
+		if(num_rows > mrows){
+			nrows = num_rows;
+		}
+		else{
+			nrows = mrows;
+		}
+		if(num_cols > mcols){
+			ncols = num_cols;
+		}
+		else{
+			ncols = mcols;
+		}
+		double * new_mat = new double[nrows * ncols];
+		#pragma omp parallel for schedule(dynamic)
+		for(unsigned int r = 0; r < nrows; r++){
+			for(unsigned int c = 0; c < ncols; c++){
+				double v = (get_elem(r, c) + mat.get_elem(r, c));
+				uint i = (r * ncols) + c;
+				new_mat[i] = v;
 			}
-			result.set_element(r, c, summ);
+		}
+		matrix n(nrows, ncols, new_mat);
+		delete[] new_mat;
+		return n;
+	}
+	matrix matrix::dot(matrix &mat){
+		uint nrows = num_rows; // Get number of rows for resulting matrix
+		uint ncols = mat.col_count(); // Get number of columns for resulting matrix
+		uint m; // m should be the minimum of the set (this matrix's columns and mat's rows)
+		double * new_mat = new double[nrows * ncols];
+		if(num_cols < mat.row_count()){
+			m = num_cols;
+		}
+		else{
+			m = mat.row_count();
+		}
+		// Algorithm for dot product is done in parallel.
+		#pragma omp parallel for schedule(dynamic)
+		for(unsigned int r = 0; r < nrows; r++){
+			for(unsigned int c = 0; c < ncols; c++){
+				double summ = 0;
+				for(unsigned int k = 0; k < m; k++){
+					summ += (get_elem(r, k) * mat.get_elem(k, c));
+				}
+				uint i = (r * ncols) + c;
+				new_mat[i] = summ;
+			}
+		}
+		matrix n(nrows, ncols, new_mat);
+		delete[] new_mat;
+		return n;
+	}
+	matrix matrix::ident(uint n){
+		double * new_mat = new double[n * n];
+		#pragma omp parallel for schedule(dynamic)
+		for(uint r = 0; r < n; r++){
+			for(uint c = 0; c < n; c++){
+				uint i = (r * n) + c;
+				if(r == c){
+					new_mat[i] = 1;
+				}
+				else{
+					new_mat[i] = 0;
+				}
+			}
+		}
+		matrix mat(n, n, new_mat);
+		delete[] new_mat;
+		return mat;
+	}
+	/*matrix matrix::invert(){
+		// to-do
+	}*/
+	matrix matrix::scalar_add(double scalar){
+		double * new_mat = new double[num_elem];
+		#pragma omp parallel for schedule(dynamic)
+		for(uint i = 0; i < num_elem; i++){
+			new_mat[i] = m[i] + scalar;
+		}
+		matrix n(num_rows, num_cols, new_mat);
+		delete[] new_mat;
+		return n;
+	}
+	matrix matrix::scalar_add(long long scalar){
+		double * new_mat = new double[num_elem];
+		#pragma omp parallel for schedule(dynamic)
+		for(uint i = 0; i < num_elem; i++){
+			new_mat[i] = m[i] + scalar;
+		}
+		matrix n(num_rows, num_cols, new_mat);
+		delete[] new_mat;
+		return n;
+	}
+	matrix matrix::scalar_mult(double scalar){
+		double * new_mat = new double[num_elem];
+		#pragma omp parallel for schedule(dynamic)
+		for(uint i = 0; i < num_elem; i++){
+			new_mat[i] = m[i] * scalar;
+		}
+		matrix n(num_rows, num_cols, new_mat);
+		delete[] new_mat;
+		return n;
+	}
+	matrix matrix::scalar_mult(long long scalar){
+		double * new_mat = new double[num_elem];
+		#pragma omp parallel for schedule(dynamic)
+		for(uint i = 0; i < num_elem; i++){
+			new_mat[i] = m[i] * scalar;
+		}
+		matrix n(num_rows, num_cols, new_mat);
+		delete[] new_mat;
+		return n;
+	}
+	matrix matrix::scalar_sub(double scalar){
+		double * new_mat = new double[num_elem];
+		#pragma omp parallel for schedule(dynamic)
+		for(uint i = 0; i < num_elem; i++){
+			new_mat[i] = m[i] - scalar;
+		}
+		matrix n(num_rows, num_cols, new_mat);
+		delete[] new_mat;
+		return n;
+	}
+	matrix matrix::scalar_sub(long long scalar){
+		double * new_mat = new double[num_elem];
+		#pragma omp parallel for schedule(dynamic)
+		for(uint i = 0; i < num_elem; i++){
+			new_mat[i] = m[i] - scalar;
+		}
+		matrix n(num_rows, num_cols, new_mat);
+		delete[] new_mat;
+		return n;
+	}
+	matrix matrix::sub(matrix &mat){
+		uint mrows = mat.row_count();
+		uint mcols = mat.col_count();
+		double * new_mat;
+		uint nrows;
+		uint ncols;
+		if(num_rows > mrows){
+			nrows = num_rows;
+		}
+		else{
+			nrows = mrows;
+		}
+		if(num_cols > mcols){
+			ncols = num_cols;
+		}
+		else{
+			ncols = mcols;
+		}
+		new_mat = new double[nrows * ncols];
+		
+		#pragma omp parallel for schedule(dynamic)
+		for(unsigned int r = 0; r < nrows; r++){
+			for(unsigned int c = 0; c < ncols; c++){
+				double v = (get_elem(r, c) - mat.get_elem(r, c));
+				uint i = (r * ncols) + c;
+				new_mat[i] = v;
+			}
+		}
+		matrix n(nrows, ncols, new_mat);
+		delete[] new_mat;
+		return n;
+	}
+	matrix matrix::transpose(){
+		matrix new_matrix(num_cols, num_rows);
+		for(uint r = 0; r < num_rows; r++){
+			for(uint c = 0; c < num_cols; c++){
+				new_matrix.set_elem(c, r, get_elem(r, c));
+			}
+		}
+		return new_matrix;
+	}
+	
+	std::string matrix::to_string(){
+		std::string str = "";
+		for(uint r = 0; r < num_rows; r++){
+			for(uint c = 0; c < num_cols; c++){
+				uint i = (r * num_cols) + c;
+				str += std::to_string(m[i]) + "\t";
+			}
+			if(r < (num_rows - 1))
+				str += "\n";
+		}
+		return str;
+	}
+	std::string matrix::get_type(){
+		return "matrix";
+	}
+	
+	uint * matrix::get_dim(){
+		uint * dimen = new uint[2];
+		dimen[0] = num_rows;
+		dimen[1] = num_cols;
+		return dimen;
+	}
+	uint matrix::row_count(){
+		return num_rows;
+	}
+	uint matrix::col_count(){
+		return num_cols;
+	}
+	
+	void matrix::set_elem(uint index, double val){
+		if(index < num_elem){
+			m[index] = val;
 		}
 	}
-	return result;
-}
-
-matrix matrix::sub(const matrix &matr){
-	uint res_rows = rows;
-	uint res_cols = cols;
-	if(res_rows < matr.rows){
-		res_rows = matr.rows;
-	}
-	if(res_cols < matr.cols){
-		res_cols = matr.cols;
-	}
-	matrix result(res_rows, res_cols);
-	#pragma omp parallel for schedule(dynamic)
-	for(uint r = 0; r < res_rows; r++){
-		for(uint c = 0; c < res_cols; c++){
-			double v = get_element(r, c) - matr.get_element(r, c);
-			result.set_element(r, c, v);
+	void matrix::set_elem(uint r, uint c, double val){
+		if(r < num_rows && c < num_cols){
+			uint i = (r * num_cols) + c;
+			m[i] = val;
 		}
 	}
-	return result;
-}
-
-matrix matrix::transpose(){
-	matrix result(cols, rows);
-	#pragma omp parallel for schedule(dynamic)
-	for(uint r = 0; r < rows; r++){
-		for(uint c = 0; c < cols; c++){
-			double v = get_element(r, c);
-			result.set_element(c, r, v);
+	matrix& matrix::operator=(const matrix &matr){
+		delete[] m;
+		num_rows = matr.num_rows;
+		num_cols = matr.num_cols;
+		num_elem = matr.num_elem;
+		m = new double[num_elem];
+		for(uint i = 0; i < num_elem; i++){
+			m[i] = matr.m[i];
 		}
+		return *this;
 	}
-	return result;
-}
-
-double matrix::get_element(uint r, uint c){
-	uint index = (cols * r) + c;
-	return mat[index];
-}
-
-void matrix::set_element(uint r, uint c, double v){
-	uint index = (cols * r) + c;
-	mat[index] = v;
-}
-
-uint matrix::num_rows(){
-	return rows;
-}
-uint matrix::num_cols(){
-	return cols;
 }
