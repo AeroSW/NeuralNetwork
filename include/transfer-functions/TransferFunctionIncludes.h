df44433409d1c5aa12b558a6bc0d6e4b442a50f3
Kenneth Cornett <kecornett42@students.tntech.edu>
2017-03-06 22:28:40 -0600

    Header file containing includes for the other transfer function headers
    Header file which contains the includes for:
    TransferFunction.h
    SigmoidFunction.h
    HyperbolicTangentFunction.h

---
+++ include/transfer-functions/TransferFunctionIncludes.h
@@ -0,0 +1,8 @@
+#ifndef TRANSFERFUNCTIONINCLUDES_H
+#define TRANSFERFUNCTIONINCLUDES_H
+
+#include "HyperbolicTangentFunction.h"
+#include "SigmoidFunction.h"
+#include "TransferFunction.h"
+
+#endif
