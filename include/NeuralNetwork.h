f34b82d2d9a563853d0afacc4a1e76727c5905a0
Kenneth Cornett <kecornett42@students.tntech.edu>
2017-03-15 09:57:19 -0500

    Defined a neural_network architecture.

---
+++ include/NeuralNetwork.h
@@ -0,0 +1,37 @@
+#ifndef NEURALNETWORK_H
+#define NEURALNETWORK_H
+
+#include "Neuron.h"
+#include "Matrix.h"
+#include "TypeDefs.h"
+
+#include <vector>
+#include <string>
+
+class neural_network{
+	private:
+		std::vector<neuron> neurons;
+		uint * structure;
+		uint num_layers;
+		
+	public:
+		neural_network(uint * blueprint, uint bp_sze);
+		neural_network(const neural_network &net);
+		~neural_network();
+		
+		matrix feed(matrix &inputs);
+		
+		double get_neuron_bias(uint l_index, uint n_index);
+		double get_neuron_weight(uint l_index, uint n_index, uint w_index);
+		matrix get_neuron_weights(uint l_index, uint n_index);
+		
+		void set_neuron_bias(uint l_index, uint n_index, double b);
+		void set_neuron_weight(uint l_index, uint n_index, uint w_index, double wt);
+		void set_neuron_weights(uint l_index, uint n_index, matrix &new_wts);
+		
+		uint num_layers();
+		uint num_nodes();
+		uint num_nodes(uint l_index);
+};
+
+#endif
